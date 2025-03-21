82b13336436e48f43b776db02221d4208c78502c
marek-tokarz <marek.tokarz14@gmail.com>
2025-03-21 19:55:38 +0100

    First simple tests of a google.com in JS with Playwright - testing a title of a webpage

---
+++ tests/google.spec.js
@@ -0,0 +1,16 @@
+const {test,expect} =require('@playwright/test')
+
+test("Verify Application Title",async function({page}){
+
+    await page.goto("http://google.com")
+
+    const url=await page.url()
+
+    console.log("The title is "+url)
+
+    const title=await page.title()
+
+    console.log("Title is "+title)
+
+    await expect(page).toHaveTitle("Google")
+})
